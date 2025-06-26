1e1f196c885c75cc43fef881cde268f0972dd4b0
Goku_007 <sadhuprem361@gmail.com>
2025-06-26 22:26:32 +0530

    Add files via upload

---
+++ calcu/calculater.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (b === 0) return "Cannot divide by zero";
+  return a / b;
+}
+
+function calculate(operation) {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  let result;
+
+  switch (operation) {
+    case "add":
+      result = add(num1, num2);
+      break;
+    case "subtract":
+      result = subtract(num1, num2);
+      break;
+    case "multiply":
+      result = multiply(num1, num2);
+      break;
+    case "divide":
+      result = divide(num1, num2);
+      break;
+  }
+
+  document.getElementById("result").innerText = "Result: " + result;
+}
